4b16ea4b87df4cfd20cba4fc7ce77df79df78a17
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2016-01-14 19:51:26 +1300

    Test: unit test for setting date format

---
+++ tests/unit/SettingDateFormatTest.php
@@ -0,0 +1,36 @@
+<?php
+
+class SettingDateFormatTest extends \Codeception\TestCase\Test
+{
+    protected $timestamp = 'January 14th, 2016 2:42 PM';
+
+    public function testChangeDateFormat()
+    {
+        /** @var \Tinyissue\Services\SettingsManager $settings */
+        $settings = app('tinyissue.settings');
+        $format1 = $settings->getDateFormat();
+        $date1 = \Html::date($this->timestamp);
+
+        $this->assertEquals($date1, $this->getDate($format1));
+
+        $format2 = 'Y-m-d';
+        $settings->save([
+            'date_format' => $format2
+        ]);
+
+        $this->assertNotEquals($date1, $this->getDate($format2));
+        $this->assertEquals(\Html::date($this->timestamp), $this->getDate($format2));
+    }
+
+    /**
+     * Returns formatted date
+     *
+     * @param $format
+     * @return string
+     */
+    protected function getDate($format)
+    {
+        $date = new \DateTime($this->timestamp);
+        return $date->format($format);
+    }
+}
