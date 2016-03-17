a19fd48d6be7b48b0097e4d937ad29cb469f7dde
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2016-03-17 20:35:43 +1300

    Change: added language package
    overtrue/laravel-lang
    Issue #17

--- config/app.php
+++ config/app.php
@@ -117,7 +117,7 @@ return [
         'Illuminate\Redis\RedisServiceProvider',
         'Illuminate\Auth\Passwords\PasswordResetServiceProvider',
         'Illuminate\Session\SessionServiceProvider',
-        'Illuminate\Translation\TranslationServiceProvider',
+        'Overtrue\LaravelLang\TranslationServiceProvider',
         'Illuminate\Validation\ValidationServiceProvider',
         'Illuminate\View\ViewServiceProvider',
         /*
